7a89877c0ab9f713682466d6173a41ded063fc57
Hex358 // Pantone 358C <165776088+hex358@users.noreply.github.com>
2025-09-16 20:37:37 +0500

    readme and license

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
-import json
-from interactions import Client, Intents, listen, slash_command, SlashContext
-config = json.load(open("../config.json"))
+# Test commands were here.
 
-bot = Client(intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT)
 
-@slash_command(name="my_command", description="My first command :)")
-async def my_command_function(ctx: SlashContext):
-    await ctx.send("Hello World")
-
-bot.start(config["token"])
+# import json
+# from interactions import Client, Intents, listen, slash_command, SlashContext
+# config = json.load(open("../config.json"))
+#
+# bot = Client(intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT)
+#
+# @slash_command(name="my_command", description="My first command :)")
+# async def my_command_function(ctx: SlashContext):
+#     await ctx.send("Hello World")
+#
+# bot.start(config["token"])
